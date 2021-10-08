b9f372efaf0d696365e1f4742d41aff88cabb645
ilya <ilya@wizer-training.com>
2021-10-08 23:33:40 +0300

    update a todo route

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.get('/todos', async(req, res) => {
         const allTodos = await pool.query('SELECT * FROM todo')
         res.json(allTodos.rows)
     } catch(err) {
-        console.err(err.message)
+        console.error(err.message)
     }
 })
 
@@ -40,14 +40,27 @@ app.get('/todos/:id', async(req, res) => {
         const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', 
             [id]
         );
-        
+
         res.json(todo.rows[0])
     } catch(err) {
-        console.log(err.message)
+        console.error(err.message)
     }
 })
 
 // update a todo
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
+            [description, id]
+        );
+
+        res.json('Todo was updated!');
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 
 // delete a todo
 
