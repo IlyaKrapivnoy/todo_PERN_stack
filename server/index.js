f76361ea54e1d60b55a4b432b48a8622bdb1d445
ilya <ilya@wizer-training.com>
2021-10-08 23:05:57 +0300

    create a first route

--- server/index.js
+++ server/index.js
@@ -7,6 +7,30 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+// ROUTES
+
+// create a todo
+app.post('/todos', async(req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)", 
+            [description]
+        );
+
+        res.json(newTodo)
+    } catch(err) {
+        console.log(err.message)
+    }
+})
+
+// get all todos
+
+// get a todo
+
+// update a todo
+
+// delete a todo
+
 app.listen(5000, () => {
     console.log('Server has started on port 5000')
 })
