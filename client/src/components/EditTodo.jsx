6d1978c96c7c625b47908f54d0c5506439e7bf0d
ilya <ilya@wizer-training.com>
2021-10-09 15:08:04 +0300

    implement edit func

--- client/src/components/EditTodo.jsx
+++ client/src/components/EditTodo.jsx
@@ -3,6 +3,25 @@ import { useState } from 'react';
 const EditTodo = ({ todo }) => {
     const [description, setDescription] = useState(todo.description);
 
+    const updateDescription = async (e) => {
+        e.preventDefault();
+        try {
+            const body = { description };
+            const response = await fetch(
+                `http://localhost:5000/todos/${todo.todo_id}`,
+                {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(body),
+                }
+            );
+
+            window.location = '/';
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
     return (
         <>
             <button
@@ -33,6 +52,7 @@ const EditTodo = ({ todo }) => {
                                 type='text'
                                 className='form-control'
                                 value={description}
+                                onChange={(e) => setDescription(e.target.value)}
                             />
                         </div>
 
@@ -41,6 +61,7 @@ const EditTodo = ({ todo }) => {
                                 type='button'
                                 class='btn btn-warning'
                                 data-dismiss='modal'
+                                onClick={(e) => updateDescription(e)}
                             >
                                 Edit
                             </button>
